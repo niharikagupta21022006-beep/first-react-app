0652966c0bf67f3dbb46746cd4a04e0e47f1c264
niharikagupta21022006-beep <you@example.com>
2026-01-19 16:50:22 +0530

    react

--- 1-projects/1-todo-app-version-1/src/App.jsx
+++ 1-projects/1-todo-app-version-1/src/App.jsx
@@ -1,50 +1,24 @@
+import AppName from "./components/AppName";
+
+import AddTodo from "./components/AddTodo";
+import TodoItem1 from "./components/TodoItem1";
+import TodoItem2 from "./components/TodoItem2";
+import './App.css';
+
+
 function App() {
+  return (
+    <center className="todo-container">
+      <AppName/>
+      <AddTodo/>
+    <div className="items-container">
+      <TodoItem1></TodoItem1>
+      <TodoItem2></TodoItem2>
+    </div>    
+        
+      </center>
+  )
   
-  return <center class ="todo-container">
-    <h1>Todo App</h1>
-    <div class="container text-center">
-  
-      <div class="row">
-        <div class="col-6">
-          <input type="text" placeholder="Enter Todo Here"></input>
-        </div>
-        <div class="col-4">
-          <input type="date"></input>
-        </div>
-        <div className="col-2">
-          <button type="button" class="btn btn-success">Add</button>
-        </div>
-      </div>
+  }
 
-      
-      <div class="row">
-        <div class="col-6">
-          Buy Milk
-        </div>
-        <div class="col-4">
-          18/1/26
-        </div>
-        <div className="col-2">
-          <button type="button" class="btn btn-danger">Delete</button>
-
-        </div>
-      </div>
-
-      
-      <div class="row">
-        <div class="col-6">
-          Go to College
-        </div>
-        <div class="col-4">
-          18/1/26
-        </div>
-        <div className="col-2">
-          <button type="button" class="btn btn-danger">Delete</button>
-
-        </div>
-      </div>
-    </div>
-  </center>
-}  
-
-export default App
+  export default App;
