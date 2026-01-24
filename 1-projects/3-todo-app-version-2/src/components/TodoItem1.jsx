05389d6c80ad3b5cb6cda4bc601c858e5cc79d2d
niharikagupta21022006-beep <you@example.com>
2026-01-24 11:37:11 +0530

    react

---
+++ 1-projects/3-todo-app-version-2/src/components/TodoItem1.jsx
@@ -0,0 +1,19 @@
+function TodoItem1(){
+
+    let todoName = "Buy Milk";
+    let todoDate = "19/1/26"
+    return(
+        <div class ="container">
+        <div class="row Kg-row">
+          <div class="col-4">{todoName}</div>
+          <div class="col-4">{todoDate}</div>
+          <div class="col-2">
+           <button type="button" class="btn btn-danger Kg-button">Delete</button> 
+            </div> 
+        </div>
+        </div>
+    )
+
+
+}
+export default TodoItem1;
