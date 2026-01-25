5fdd412c13f724f207661f92d6f33914aebeb0b8
niharikagupta21022006-beep <you@example.com>
2026-01-25 09:53:59 +0530

    react

--- 1-projects/3-todo-app-version-2/src/App.jsx
+++ 1-projects/3-todo-app-version-2/src/App.jsx
@@ -1,19 +1,31 @@
 import AppName from "./components/AppName";
 
 import AddTodo from "./components/AddTodo";
-import TodoItem1 from "./components/TodoItem1";
-import TodoItem2 from "./components/TodoItem2";
+import TodoItem from "./components/TodoItem";
 import './App.css';
 
 
 function App() {
+
+  const todoItems = [
+    {
+      name:"Buy Milk",
+      dueDate:"19/1/26",
+    },
+
+    {
+      name:"Go to College",
+      dueDate:"19/1/26",
+    },
+  ];
   return (
     <center className="todo-container">
       <AppName/>
       <AddTodo/>
     <div className="items-container">
-      <TodoItem1></TodoItem1>
-      <TodoItem2></TodoItem2>
+      <TodoItem todoDate="19/1/26" todoName="Buy Milk"></TodoItem>
+      <TodoItem todoDate="19/1/26" todoName="Go to College"></TodoItem>
+
     </div>    
         
       </center>
---
+++ 1-projects/3-todo-app-version-2/src/components/TodoItem.jsx
@@ -0,0 +1,16 @@
+function TodoItem({todoName,todoDate}){
+    return(
+        <div className ="container">
+        <div className="row Kg-row">
+          <div className="col-4">{todoName}</div>
+          <div className="col-4">{todoDate}</div>
+          <div className="col-2">
+           <button type="button" class="btn btn-danger Kg-button">Delete</button> 
+            </div> 
+        </div>
+        </div>
+    )
+
+
+}
+export default TodoItem;
